87582d1652a797497050e29a1932c758668332ad
ben91lin <ben91lin@gmail.com>
2021-04-02 00:58:58 +0800

    change datastructure.

--- src/ptt_scraper/config.py
+++ src/ptt_scraper/config.py
@@ -1,8 +1,6 @@
-config = {
-    'baseUrl': 'https://www.ptt.cc',
-    'url': 'https://www.ptt.cc/bbs/Beauty/index.html',
-    'boardName' 'Beauty',
-    'headers': {
-        'cookie': 'over18=1;'
-        }
+baseUrl = 'https://www.ptt.cc'
+url = 'https://www.ptt.cc/bbs/Beauty/index.html'
+boardName = 'Beauty'
+headers = {
+    'cookie': 'over18=1;'
 }
