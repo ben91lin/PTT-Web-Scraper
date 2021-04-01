9a8cb95bbf278d568a9fc88fce079a17bc56728e
ben91lin <ben91lin@gmail.com>
2021-04-02 00:49:36 +0800

    buf fix & make more clear

--- src/ptt_scraper/board_viewer.py
+++ src/ptt_scraper/board_viewer.py
@@ -1,13 +1,14 @@
-import logger
+import logging
+import re
 import requests
 import typing as t
 from bs4 import BeautifulSoup
 from abstract.board_viewer import BoardViewer
 
 class PttBoardViewer(BoardViewer):
-    BASE_URL = 'https://www.ptt.cc'
+    BASE_URL = 'https://www.ptt.cc/bbs/'
     SELECTOR = {
-        'NAV_BUTTONS': 'div.#action-bar-container div.btn-group-paging > a',
+        'NAV_BUTTONS': 'div#action-bar-container div.btn-group-paging > a',
         'POSTS': 'div.r-list-container div.r-ent',
         'POST_META': {
             'DATE': 'div.meta > div.date',
@@ -17,34 +18,61 @@ class PttBoardViewer(BoardViewer):
             'URL': 'div.title > a'
         }
     }
+    RE = {
+        'url': re.compile(r'^https:\/\/(www\.)?ptt\.cc\/bbs\/[a-zA-Z0-9]+\/index[0-9]*\.html$')
+    }
 
-    def __init__(self, url: str, headers: dict, BeautifulSoup = BeautifulSoup):
-        self.soup = BeautifulSoup(markup = self.__get(url, headers), features = 'html.parser')
+    def __init__(self, headers: dict):
+        self.status = {
+            'headers': headers,
+        }
+        self.soup = None
 
-    def __get(self, url: str, headers: dict):
+    def get(self, url: str, **headers: dict) -> t.Optional[Exception]:
+        if not self.__check_url(url):
+            raise Exception(f'Request url: {url} is not PTT board.')
+
         req = requests.get(url, headers = headers)
+        self.__set_status(url, req)
 
-        if (req.status_code != 200):
-            raise Exception(f'The request status is {req.status_code}')
+        if not self.__sucess(req):
+            raise Exception(f'Request to {url} is failed, response status is {req.status_code}')
         else:
-            return req.text
+            print(f'[INFO] {url} success')
+    
+    def __check_url(self, url: str) -> bool:
+        if re.match(self.RE['url'], url):
+            return True
+        else:
+            return False
 
-    def prev_page(self) -> t.Union[None or str]:
+    def __sucess(self, req: requests) -> bool:
+        return req.status_code == 200
+
+    def __set_status(self, url: str, req: requests) -> None:
+        self.status['req_url'] = url
+        self.status['res_code'] = req.status_code
+        if self.__sucess(req):
+            self.soup = BeautifulSoup(markup = req.text, features = 'html.parser')
+        else:
+            self.soup = None
+            
+    def prev_page(self) -> t.Optional[str]:
         prevElement = self.soup.select(self.SELECTOR.get("NAV_BUTTONS"))[1]
         if self.__is_disable(prevElement):
             return None
         else:
-            return f'{self.BASE_URL}{nextElement["href"]}'
+            return f'{self.BASE_URL}{prevElement["href"]}'
 
-    def next_page(self) -> t.Union[None or str]:
+    def next_page(self) -> t.Optional[str]:
         nextElement = self.soup.select(self.SELECTOR.get("NAV_BUTTONS"))[2]
         if self.__is_disable(nextElement):
             return None
         else:
             return f'{self.BASE_URL}{prevElement["href"]}'
 
-    def __is_disable(self, linkSoup) -> bool:
-        return 'disabled' in linkSoup['class']
+    def __is_disable(self, soup) -> bool:
+        return 'disabled' in soup['class']
 
     def oldest_page(self) -> str:
         return f'{self.BASE_URL}{self.soup.select(self.SELECTOR.get("NAV_BUTTONS"))[0]["href"]}'
@@ -60,13 +88,15 @@ class PttBoardViewer(BoardViewer):
             if (self.__is_deleted(post)):
                 continue
 
-            date = post.select(self.SELECTOR.get("POST_META").get("DATE"))[0].text
-            author = post.select(self.SELECTOR.get("POST_META").get("AUTHOR"))[0].text
-            title = self.__regular_title(post.select(self.SELECTOR.get("POST_META").get("TITLE"))[0].text)
-            pushNumber = self.__numerate_push(post.select(self.SELECTOR.get("POST_META").get("PUSH_NUMBER"))[0].text)
-            url =  f'{self.Domain}{post.select(self.SELECTOR.get("POST_META").get("URL"))[0]["href"]}'
-
-            postMetas.append({'url': url, 'author': author, 'title': title, 'date': date, 'pushNumber': pushNumber})
+            postMetas.append(
+                {
+                    'url': post.select(self.SELECTOR.get("POST_META").get("DATE"))[0].text,
+                    'author': post.select(self.SELECTOR.get("POST_META").get("AUTHOR"))[0].text,
+                    'title': self.__regular_title(post.select(self.SELECTOR.get("POST_META").get("TITLE"))[0].text),
+                    'date': self.__numerate_push(post.select(self.SELECTOR.get("POST_META").get("PUSH_NUMBER"))[0].text),
+                    'pushNumber': f'{self.BASE_URL}{post.select(self.SELECTOR.get("POST_META").get("URL"))[0]["href"]}'
+                }
+                )
 
         return postMetas
 
@@ -77,8 +107,8 @@ class PttBoardViewer(BoardViewer):
         if pushNumber == '爆': return 100
         if pushNumber == '': return 0
         if pushNumber == 'XX': return -100
-        if pushNumber[0] == 'X': return -(Int(pushNumber[0]) * 10)
-        return pushNumber
+        if pushNumber[0] == 'X': return -(int(pushNumber[1]) * 10)
+        return int(pushNumber)
 
     def __regular_title(self, title: str) -> str:
-        return re.sub('[\/*?"<>|:"\s\n]', '', title)
+        return re.sub(r'[\/*?"<>|:"\s\n]', '', title)
