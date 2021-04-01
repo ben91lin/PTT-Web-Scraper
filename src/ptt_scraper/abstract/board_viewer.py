ff80462d2c158a0b8e0526436379ec77d64a5cbb
ben91lin <ben91lin@gmail.com>
2021-04-02 00:50:23 +0800

    import abc for abstract class.

--- src/ptt_scraper/abstract/board_viewer.py
+++ src/ptt_scraper/abstract/board_viewer.py
@@ -1,18 +1,34 @@
-class BoardViewer:
+import abc
 
-    def __init__() -> None:
-        # Initialize BoardViewer.
-        pass
-    def __get() -> None:
+class BoardViewer(abc.ABC):
+
+    @abc.abstractmethod
+    def get() -> None:
         # Get HTML doc. Storage in property.
-        pass
+        return NotImplemented
+
+    @abc.abstractmethod
     def prev_page() -> str:
         # Return PrevPage link.
-        pass
+        return NotImplemented
+
+    @abc.abstractmethod
     def next_page() -> str:
         # Return nextPage link.
-        pass
+        return NotImplemented
+
+    @abc.abstractmethod
+    def oldest_page(self) -> str:
+        # Return oldestPage link.
+        return NotImplemented
+
+    @abc.abstractmethod
+    def newest_page(self) -> str:
+        # Return newestPage link.
+        return NotImplemented
+
+    @abc.abstractmethod
     def get_posts() -> list:
         # Return postMetas.
         # Include post url, author, date, pushNum .etc
-        pass
+        return NotImplemented
