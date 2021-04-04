af31f11013f257cb155ab4f77ac6edc0b8a659ca
ben91lin <ben91lin@gmail.com>
2021-04-04 22:17:30 +0800

    change attr, method private -> protect

--- src/ptt_scraper/connection.py
+++ src/ptt_scraper/connection.py
@@ -7,58 +7,58 @@ from bs4 import BeautifulSoup
 class Connection():
 
     def __init__(self, headers: dict = None) -> None:
-        self.__original_headers = headers or None
-        self.__soup = None
-        self.__status = {
+        self._original_headers = headers or None
+        self._soup = None
+        self._status = {
             'request_url': None,
-            'headers': self.__original_headers,
+            'headers': self._original_headers,
             'response_code': None,
             'looking_for': None,
         }
 
     def get(self, url: str, headers: t.Optional[dict] = None):
-        self.__reset_status()
-        if not self.__check_url(url): 
+        self._reset_status()
+        if not self._check_url(url): 
             raise Exception(f'This url is not a PTT link.')
 
-        headers = headers if headers is not None else self.__status['headers']
+        headers = headers if headers is not None else self._status['headers']
         request = requests.get(url, headers = headers)
-        self.__set_status(url, headers, request)
+        self._set_status(url, headers, request)
 
-        if self.__sucess(request):
-            self.__soup = BeautifulSoup(markup = request.text, features = 'html.parser')
+        if self._sucess(request):
+            self._soup = BeautifulSoup(markup = request.text, features = 'html.parser')
             print(f'[INFO] {url} success')
         else:
             self.soup = None
             raise Exception(f'Request to {url} is failed, response status is {req.status_code}')
 
-    def __reset_status(self) -> None:
-        self.__status['headers'] = self.__original_headers
-        self.__status['request_url'] = None
-        self.__status['response_code'] = None
-        self.__status['looking_for'] = None
+    def _reset_status(self) -> None:
+        self._status['headers'] = self._original_headers
+        self._status['request_url'] = None
+        self._status['response_code'] = None
+        self._status['looking_for'] = None
 
-    def __check_url(self, url: str) -> bool:
+    def _check_url(self, url: str) -> bool:
         match = re.match(
             r'^https://(www\.)?ptt\.cc/bbs/[a-zA-Z0-9]+/(index[0-9]*\.html$|M\.[0-9]{10}\.A\.[A-Z0-9]{3}\.html$)',
             url
             )
         return True if match else False
 
-    def __set_status(self, url: str, headers: dict, request: requests) -> None:
-        self.__status['request_url'] = url
-        self.__status['headers'] = headers
-        self.__status['response_code'] = request.status_code
-        self.__status['looking_for'] = self.__parse_url(url)
+    def _set_status(self, url: str, headers: dict, request: requests) -> None:
+        self._status['request_url'] = url
+        self._status['headers'] = headers
+        self._status['response_code'] = request.status_code
+        self._status['looking_for'] = self._parse_url(url)
 
-    def __sucess(self, req: requests) -> bool:
+    def _sucess(self, req: requests) -> bool:
         return req.status_code == 200
 
-    def __parse_url(self, url: str) -> tuple:
+    def _parse_url(self, url: str) -> tuple:
         url = url.split('/')
         genre = 'board' if 'index' in url.pop() else 'article'
         board = url.pop()
         return board, genre
 
     def __repr__(self):
-        return f'{self.__status}'
+        return f'{self._status}'
--- tests/test_connection.py
+++ tests/test_connection.py
@@ -26,7 +26,7 @@ def test_instance(instance):
 )
 
 def test__check_url(instance, url, expected):
-    assert instance._Connection__check_url(url) == expected
+    assert instance._check_url(url) == expected
 
 def test_looking_for_board(instance):
     status_for_board = {
@@ -38,7 +38,7 @@ def test_looking_for_board(instance):
         'looking_for': ('Beauty', 'board')
     }
     instance.get('https://www.ptt.cc/bbs/Beauty/index3614.html')
-    assert instance._Connection__status == status_for_board
+    assert instance._status == status_for_board
 
 def test_looking_for_article(instance):
     status_for_article = {
@@ -51,11 +51,11 @@ def test_looking_for_article(instance):
         'https://www.ptt.cc/bbs/Beauty/M.1617413202.A.764.html',
         headers = {}
         )
-    assert instance._Connection__status == status_for_article
+    assert instance._status == status_for_article
 
 def test__reset_status(instance):
     instance.get('https://www.ptt.cc/bbs/Beauty/M.1617413202.A.764.html')
-    instance._Connection__reset_status()
+    instance._reset_status()
     status_for_board = {
         'headers': {
             'cookie': 'over18=1;' 
@@ -64,4 +64,15 @@ def test__reset_status(instance):
         'response_code': None,
         'looking_for': None
     }
-    assert instance._Connection__status == status_for_board
+    assert instance._status == status_for_board
+
+@pytest.mark.parametrize(
+    "url",
+    [
+        'https://www.ptt.cc/bbs/Beauty/index.htmlasd',
+        'https://www.ptt.cc/bbs/Beauty/index12312.html'
+    ]
+)
+def test_get_exception(instance, url):
+    with pytest.raises(Exception):
+        instance.get(test_case)
