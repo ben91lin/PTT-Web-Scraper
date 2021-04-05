52376f95c0bfcc5f2fab4946455672e0682babce
ben91lin <ben91lin@gmail.com>
2021-04-06 01:28:53 +0800

    full article html test

--- src/ptt_scraper/article_viewer.py
+++ src/ptt_scraper/article_viewer.py
@@ -1,3 +1,4 @@
+import copy
 import logging
 import re
 import requests
@@ -11,7 +12,7 @@ class PTTArticleViewer(Connection, ArticleViewer):
     SELECTOR = {
         'ARTICLE_META': {
             # In html.parser, the nth-of-type will recaculate article-metaline and article-metaline-right.
-            'ALL': 'div.article-metaline, div.article-metaline-right, span.f2',
+            'ALL': 'div.article-metaline, div.article-metaline-right',
             'AUTHOR': 'div.article-metaline:nth-of-type(1) span.article-meta-value',
             'BOARD': 'div.article-metaline-right span.article-meta-value',
             'TITLE': 'div.article-metaline:nth-of-type(3) span.article-meta-value',
@@ -19,6 +20,8 @@ class PTTArticleViewer(Connection, ArticleViewer):
         },
         'CONTENT': 'div#main-content',
         'COMMENTS': 'span.f2 ~ div.push',
+        # The richcontent is iframe preview for imgur.com
+        'COMMENT_RICHCONTENTS': 'span.f2 ~ div.push ~ .richcontent',
         'COMMENT_META': {
             'TAG': 'push-tag',
             'COMMENTOR': 'push-userid',
@@ -38,17 +41,12 @@ class PTTArticleViewer(Connection, ArticleViewer):
 
     def meta(self) -> str:
         return {
-            'author': self.author(),
-            'title': self.title(),
             'datetime': self.datetime(),
-            'author_ip': self.author_ip()
+            'author_ip': self.author_ip(),
+            'author_id': self.author_id(),
+            'author_nickname': self.author_nickname(),
+            'title': self.title(),
         }
-
-    def author(self) -> str:
-        return self._soup.select(self.SELECTOR['ARTICLE_META']['AUTHOR'])[0].text
-
-    def title(self) -> str:
-        return self._soup.select(self.SELECTOR['ARTICLE_META']['TITLE'])[0].text
 
     def datetime(self) -> str:
         return self._soup.select(self.SELECTOR['ARTICLE_META']['DATETIME'])[0].text
@@ -57,23 +55,40 @@ class PTTArticleViewer(Connection, ArticleViewer):
         search = re.search(self.RE['author_ip'], self._soup.text)
         return search.group(0) if search else ''
 
-    def content(self) -> str:
-        selection = self._soup.select(self.SELECTOR['CONTENT'])
-        selection = self.__decompose_article_meta(selection)
-        selection = self.__decompose_comments(selection)
-        return selection.html
+    def author(self) -> str:
+        return self._soup.select(self.SELECTOR['ARTICLE_META']['AUTHOR'])[0].text
 
-    def __decompose_article_metas(self, selection) -> BeautifulSoup:
+    def author_id(self) -> str:
+        return self.author().split(' (')[0]
+    
+    def author_nickname(self) -> str:
+        return self.author().split(' (')[1].split(')')[0]
+
+    def title(self) -> str:
+        return self._soup.select(self.SELECTOR['ARTICLE_META']['TITLE'])[0].text
+
+    def content(self) -> str:
+        soup = BeautifulSoup(features = 'html.parser')
+        soup.append(copy.copy(self._soup.select(self.SELECTOR['CONTENT'])[0]))
+        self.__decompose_comment_richcontents(soup)
+        self.__decompose_comments(soup)
+        self.__decompose_article_meta(soup)
+        return soup.__repr__()
+
+    def __decompose_article_meta(self, selection) -> None:
         article_metas = selection.select(self.SELECTOR['ARTICLE_META']['ALL'])
         for element in article_metas:
             element.decompose()
-        return selection
 
-    def __decompose_comments(self, selection) -> BeautifulSoup:
-        comments = selection.select(self.SELECTOR['COMMENTS']).decompose()
+    def __decompose_comment_richcontents(self, selection) -> None:
+        richcontents = selection.select(self.SELECTOR['COMMENT_RICHCONTENTS'])
+        for element in richcontents:
+            element.decompose()
+
+    def __decompose_comments(self, selection) -> None:
+        comments = selection.select(self.SELECTOR['COMMENTS'])
         for element in comments:
             element.decompose()
-        return selection
 
     def comments(self) -> list[dict]:
         selections = self._soup.select(self.SELECTOR['COMMENTS'])
@@ -82,11 +97,11 @@ class PTTArticleViewer(Connection, ArticleViewer):
         for comment in selections:
             outputs.append(
                 {
+                    'datetime': self.__datetime(),
                     'tag': comment.select(self.SELECTOR['COMMENT_META']['TAG']),
-                    'commentor': comment.select(self.SELECTOR['COMMENT_META']['COMMENTOR']),
+                    'commentor_id': comment.select(self.SELECTOR['COMMENT_META']['COMMENTOR']),
+                    'commentor_ip': self.__commentor_ip(),
                     'comment': comment.select(self.SELECTOR['COMMENT_META']['COMMENT']),
-                    'ip': self.__commentor_ip(),
-                    'datetime': self.__datetime()
                 }
             )
 
--- tests/test_article_viewer.py
+++ tests/test_article_viewer.py
@@ -16,14 +16,50 @@ def test_instance(instance):
     assert isinstance(instance, PTTArticleViewer)
 
 def test_meta(article1):
-    assert article1.author() == 'MCI (步步步步步步步步)'
-    assert article1.title() == 'Re: (問題)華航空難留言'
-    assert article1.datetime() == 'Mon Jun 20 13:04:48 2005'
-    assert article1.author_ip() == '220.137.87.186'
     assert article1.meta() == {
-        'author': 'MCI (步步步步步步步步)',
-        'title': 'Re: (問題)華航空難留言',
         'datetime': 'Mon Jun 20 13:04:48 2005',
-        'author_ip': '220.137.87.186'
+        'author_ip': '220.137.87.186',
+        'author_id': 'MCI',
+        'author_nickname': '步步步步步步步步',
+        'title': 'Re: (問題)華航空難留言'
     }
+    
+def test_content(article1):
+    assert article1.content() == '''<div class="bbs-screen bbs-content" id="main-content">
+<span class="f2">※ 引述《suandfly (槍兵￼ ￼N￾ ￾ ￼ ￼NＩ》之銘言：
+</span><span class="f6">: ※ 引述《certifi (i miss you)》之銘言：
+</span><span class="f6">: : 看到這邊，就知道是垃圾
+</span><span class="f6">: : 1998年
+</span><span class="f6">: : 2月16日華航空中巴士a300-600r在中正機場重飛失敗，墜毀
+</span><span class="f6">: : 2月19日R-CH-1在白沙灣，墜毀
+</span><span class="f6">: : 2月24日t-38台東外海墜毀，駕駛跳傘逃生
+</span><span class="f6">: : 3月2日德安航空貝爾412型直昇機在澎湖外海降落於鑽油平台，失速墜毀
+</span><span class="f6">: : 3月17日陸軍輕航隊th-55型直昇機，在歸仁迫降失敗，墜毀
+</span><span class="f6">: : 3月18日國華SAAB300型客機在新竹機場離場時，於海中墜毀
+</span><span class="f6">: : 3月20日F-16-B澎湖外海墜毀
+</span><span class="f6">: : 每4年發生一次空難--又是只取有利的樣本
+</span><span class="f6">: 講到空難
+</span><span class="f6">: 我也來暴個八卦
+</span><span class="f6">: 還記得當年有一架直昇機
+</span><span class="f6">: 掉到德基水庫吧
+</span><span class="f6">: 據說....
+</span><span class="f6">: 是飛行員...學長帶著學弟兩架直昇機
+</span><span class="f6">: 去完蜻蜓點水....
+</span><span class="f6">: 結果沒點好就下去了....
+</span><span class="f6">: ORZ..
+</span>講到直升機空難 民國63年陸軍昌x演習曾經發生一次有史以來最嚴重的空難
+當時兩架直升機上包括官階最高的陸軍總司令于上將 還有好幾位中將
+總共二十幾顆星星隕落
+據說當時生還者中唯一意識清楚的是官階最小的高上尉 也是于上將的侍從官
+他負傷把于上將從殘骸中拖出來 然後跑到公路上求救
+不過于上將後半輩子半身不遂 有點像阿甘的劇情 不過于上將家人始終很感謝高
+直到于上將晚年過世後還有連絡 也有一說是幾位將軍用肉身保護總司令
+總司令才能幸運生還 至於這位高上尉後來一直升到聯勤總司令 也當了上將
+目前在某個服務老榮民的單位當主委 這位高將軍在921時也擔任中部救災指揮官
+前陣子看到新聞說他有可能是國防部長的黑馬人選
+我想可能是那些被他幫助過的人在天上保佑他吧!
 
+--
+<span class="f2">※ 發信站: 批踢踢實業坊(ptt.cc) 
+</span>◆ From: 220.137.87.186
+</div>'''
