82e01a4b3b03a7bb35181d906fc262a935d2ea7c
ben91lin <ben91lin@gmail.com>
2021-04-20 17:14:38 +0800

    storage data and simple pipeline.

--- src/ptt_scraper/datastructure.py
+++ src/ptt_scraper/datastructure.py
@@ -1,128 +1,90 @@
 import sys
 import typing as t
 
-class PttData:
-    
-    def __init__(self, boardName: str):
-        self.boardName = boardName
-        self.data = []
+class Data:
+    '''
+        Use list comprehension instead of filter(), it's faster and more clearly.
+    '''
 
-    def addPage(self, pageNumber: int, url: str):
-        self.data.push(
-            {
-                'pageNumber': pageNumber,
-                'pageUrl': url,
-                'posts': []
+    def __init__(self, name: str, data: list = []):
+        self._name = name
+        self._data = data
+        self._OPERATOR = {
+            '$all': self.all,
+            '$in': self.contain,
+            '$not_in': self.not_contain,
+            '$equal': self.equal,
+            '$==': self.equal,
+            '$not_equal': self.not_equal,
+            '$!=': self.not_equal,
+            '$greater': self.greater,
+            '$>': self.greater,
+            '$greater_and_equal': self.greater_and_equal,
+            '$>=': self.greater_and_equal,
+            '$less': self.less,
+            '$<': self.less,
+            '$less_and_equal': self.less_and_equal,
+            '$<=': self.less_and_equal,
             }
-        )
-    
-    def addPost(self, posts: property, url: str, title: str, date: str, author: str, pushNumber: int):
-        posts.push(
-            {
-                'postUrl': url,
-                'postTitle': title,
-                'postDate': date,
-                'postAuthor': author,
-                'postPushNumber': pushNumber,
-                'postContent': '',
-                'postMedias': [],
-                'pushMedias': [],
-                'pushs':[]
-            }
-        )
 
-    def addPostContent(self, postContent: property, html: str):
-        postContent = html
-    
-    def addPostMedias(self, postMedias: property, url: str):
-        postMedias.push(url)
+    @property
+    def data(self):
+        return self._data
 
-    def addPushMedias(self, PushMedias: property, url: str):
-        postMedias.push(url)
+    @data.setter
+    def data(self, d):
+            self._data = d
 
-    def addPush(self, pushs: property, pushState: int, user: str, ip: str, datetime: str):
-        pushs.push(pushState, user, ip, datetime)
+    def pipeline(self, *args):
+        '''
+        { "$operator": (key, value) }, { "$operator": (key, value) }
+        '''
+        for arg in args:
+            for k, v in arg.items():
+                self._OPERATOR[k](v[0], v[1])
 
-class TupleStorage:
-    """
-        The TupleStorage work like tuple.
-        You can use X = TupleStorage.wrap(new_attributes: tuple) to set tuple keys.
-        And use X as constructor, Y = X(data: tuple) to storage data.
-        The len(keys) should equal to len(data).
-        Can use +, ==, in operator.
-        It's iterable.
-    """
-    __hash__ = None
-    attributes: tuple[str] = ()
+    def filter(self, cb):
+        self._data = filter(cb, self._data)
+        return self
 
-    def __init__(
-        self, data: tuple
-        ) -> None:
-        self._tuple = data
+    def all(self, key: str, values: t.Union[str, list, tuple]) -> 'Data':
+        values = [values] if isinstance(values, str) else values
+        self._data = [data for data in self._data if all(v in data.get(key, '') for v in values)]
+        return self
 
-    @classmethod
-    def wrap(
-        cls, new_attributes: tuple = (), name: t.Optional[str] = None
-    ) -> t.Type["TupleStorage"]:
+    def contain(self, key: str, values: t.Union[str, list, tuple]) -> 'Data':
+        values = [values] if isinstance(values, str) else values
+        self._data = [data for data in self._data if any(v in data.get(key, '') for v in values)]
+        return self
 
-        class newcls(cls):
-            attributes = cls.attributes + tuple(map(lambda attr: attr.lower(), new_attributes))
+    def not_contain(self, key: str, values: t.Union[str, list, tuple]) -> 'Data':
+        values = [values] if isinstance(values, str) else values
+        self._data = [data for data in self._data if any(v not in data.get(key, '') for v in values)]
+        return self
 
-            def __init__(self, *args, **kwargs):
-                super().__init__(*args, **kwargs)
+    def equal(self, key: str, value) -> 'Data':
+        self._data = [data for data in self._data if data.get(key, '') == value]
+        return self
 
-        newcls.__module__ = sys._getframe(1).f_globals["__name__"] # __main__
-        name = f'{cls.__name__}-{name}' if name else cls.__name__
-        newcls.__name__ = newcls.__qualname__ = name
-        return newcls
+    def not_equal(self, key: str, value) -> 'Data':
+        self._data = [data for data in self._data if data.get(key, '') != value]
+        return self
 
-    def __add__(self, other):
-        # check if class exist.
-        subclasses = TupleStorage.__subclasses__()
-        target = self.__combine_name(self.__class__.__name__, other.__class__.__name__)
-        if (self.__has_class(subclasses, target)):
-            for sub in subclasses:
-                if sub.__name__ == target:
-                    return sub(data = (self._tuple + other._tuple))
-        else:
-            return TupleStorage.wrap(
-                new_attributes = (self.attributes + other.attributes),
-                name = target
-                )(
-                    data = (self._tuple + other._tuple)
-                )
+    def greater(self, key: str, value) -> 'Data':
+        self._data = [data for data in self._data if data.get(key, '') > value]
+        return self
 
-    def __combine_name(self, name1, name2):
-        name1 = "".join(name1.split("-")[1:])
-        name2 = "".join(name2.split("-")[1:])
-        return f'{name1}+{name2}'
+    def greater_and_equal(self, key: str, value) -> 'Data':
+        self._data = [data for data in self._data if data.get(key, '') >= value]
+        return self
 
-    def __has_class(self, subclasses, target):
-        if target in map(lambda sub: sub.__name__, subclasses):
-            return True
-        else:
-            return False
+    def less(self, key: str, value) -> 'Data':
+        self._data = [data for data in self._data if data.get(key, '') < value]
+        return self
 
-    def __eq__(self, other):
-        return other.__class__ is self.__class__ and other._tuple == self._tuple
-
-    def __getitem__(self, key):
-        if isinstance(key, str):
-            if key.lower() not in self.__class__.attributes:
-                raise KeyError(f'{key} is not in {self.__class__.name}.')
-            else:
-                return self._tuple[self.__class__.attributes.index(key.lower())]
-        return self._tuple[key]
-
-    def __iter__(self):
-        # if object is iterable, it can use 'in' operator.
-        return iter(self._tuple)
-
-    def __len__(self):
-        return len(self._tuple)
+    def less_and_equal(self, key: str, value) -> 'Data':
+        self._data = [data for data in self._data if data.get(key, '') <= value]
+        return self
 
     def __repr__(self):
-        return f'{self._tuple}'
-
-
-
+        return f'{self._name}: 共{len(self._data)}筆'
